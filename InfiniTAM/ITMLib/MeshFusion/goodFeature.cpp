b10faaf4d0e7229aa16b1011082711ef2381a7dd
toreal <toreal@gmail.com>
2016-08-31 19:44:02 +0800

    kmeans

--- InfiniTAM/ITMLib/MeshFusion/goodFeature.cpp
+++ InfiniTAM/ITMLib/MeshFusion/goodFeature.cpp
@@ -23,14 +23,60 @@ void MeshFusion::goodFeature()
 
 
 	int  clusterCount = 3;
-	int  sampleCount = 20;
-	Mat points(sampleCount, 1, CV_32FC2), labels;
+	//int  sampleCount = 20;
+	Mat labels;
 	Mat centers;
+//	Mat points2 = Mat(newPoints);
+
+	cv::Mat points(m_latest_paired_corners_curr.size(),2, CV_32FC1);
+
+	for (size_t i = 0, end = m_latest_paired_corners_curr.size(); i < end; ++i) {
+		points.at<float>(i, 0) = m_latest_paired_corners_curr[i].x;
+		points.at<float>(i, 1) = m_latest_paired_corners_curr[i].y;
+	//	points.at<float>(i, 2) = newPoints[i].z;
+	}
+
 
 	kmeans(points, clusterCount, labels,
 		TermCriteria(TermCriteria::EPS + TermCriteria::COUNT, 10, 1.0),
 		3, KMEANS_PP_CENTERS, centers);
 
+	Mat new_image(640,480,CV_8U);
+
+	for (int y = 0; y < points.rows; y++)
+	{
+		int cluster_idx = labels.at<int>(y, 0);
+		 int nx=points.at<float>(y, 0) ;
+		 int ny=points.at<float>(y, 1) ;
+
+		 new_image.at<unsigned char >(nx, ny) = 100;
+		 new_image.at<unsigned char >(nx-1, ny) =   100;
+		 new_image.at<unsigned char >(nx+1, ny) =   100;
+		 if (cluster_idx > 0)
+		 {
+			 new_image.at<unsigned char >(nx, ny - 1) =   100;
+			 new_image.at<unsigned char >(nx - 1, ny - 1) =   100;
+			 new_image.at<unsigned char >(nx + 1, ny - 1) =   100;
+			 if (cluster_idx > 1)
+			 {
+				 new_image.at<unsigned char >(nx, ny + 1) =0;
+				 new_image.at<unsigned char >(nx - 1, ny + 1) = 0;
+				 new_image.at<unsigned char >(nx + 1, ny + 1) = 0;
+			 }
+		 }
+
+	}
+		for (int x = 0; x < points.cols; x++)
+		{
+			int f1 = centers.at<float>(0, x);
+			int f2 = centers.at<float>(1, x);
+		//	float f3 = centers.at<float>(2, x);
+			new_image.at<unsigned char >(f1, f2) = 127;
+		}
 
 
+		imshow("tt", new_image);
+		waitKey(0);
+
+	return;
 }
